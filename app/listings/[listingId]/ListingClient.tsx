7201532c1d98f78db8949870407b7e87be80b7f8
LazarShockX <donaldbr12@gmail.com>
2025-08-21 14:03:58 +0200

    Hotfix: rename variable

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -34,7 +34,7 @@ export const ListingClient = ({ reservations = [], listing, currentUser }: Listi
     const loginModal = useLoginModal();
     const router = useRouter();
 
-    const disableDates = useMemo(() => {
+    const disabledDates = useMemo(() => {
         let dates: Date[] = [];
         
         reservations.forEach((reservation) => {
@@ -103,7 +103,7 @@ export const ListingClient = ({ reservations = [], listing, currentUser }: Listi
                     <div className="grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6">
                         <ListingInfo user={listing.user} category={category} description={listing.description} roomCount={listing.roomCount} guestCount={listing.guestCount} bathroomCount={listing.bathroomCount} locationValue={listing.locationValue} />
                         <div className="order-first mb-10 md:order-last md:col-span-3">
-                            <ListingReservation price={listing.price} totalPrice={totalPrice} onChangeDate={(value) => setDateRange(value)} dateRange={dateRange} onSubmit={onCreateReservation} disabled={isLoading} disabledDates={disableDates} />
+                            <ListingReservation price={listing.price} totalPrice={totalPrice} onChangeDate={(value) => setDateRange(value)} dateRange={dateRange} onSubmit={onCreateReservation} disabled={isLoading} disabledDates={disabledDates} />
                         </div>
                     </div>
                 </div>
