9593e7fb7437398f77497dadb8352ae41eecdacd
LazarShockX <donaldbr12@gmail.com>
2025-08-29 20:55:20 +0200

    Hotfix: Font weight on small button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -19,7 +19,7 @@ export const Button = ({ label, onClick, disabled, outline, small, icon: Icon }:
             className={`
                 relative disabled:opacity-70 disabled:cursor-not-allowed rounded-lg hover:opacity-80 transition active:scale-[0.98] w-full cursor-pointer
                 ${outline ? "bg-white border-black text-black" : "bg-rose-500 border-rose-500 text-white"} 
-                ${small ? "py-1 text-sm font-light border-[1px]" : "py-3 text-md font-semibold border-2"}
+                ${small ? "py-1 text-sm font-semibold border-[1px]" : "py-3 text-md font-semibold border-2"}
                 `} 
             >
             {Icon && (
